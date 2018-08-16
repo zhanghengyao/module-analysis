9af637332827cd966807233bafe7f3adf6f9d424
恒遥 <hengyao.zhy@alibaba-inc.com>
2018-08-16 10:11:53 +0800

    feat: v0.0.1 done

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const moduleAnalysis = require('./lib/core/module-analysis');
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at:', p, 'reason:', reason);
+});
 module.exports = moduleAnalysis;
--- test/fixtures/demo.js
+++ test/fixtures/demo.js
@@ -1,6 +1,65 @@
-exports.convert = function convert(str){ 
-  str = str.replace(/(\\u)(\w{1,4})/gi, (unicode) => { 
-      return (String.fromCharCode(parseInt((escape(unicode).replace(/(%5Cu)(\w{1,4})/g,"$2")),16))); 
-  });
-  return str; 
-}
+/**
+ * 移动端的 fillinfo 的 controller
+ * @type {[type]}
+ */
+module.exports = app => {
+  return class MobileFillInfoController extends app.BaseController {
+
+    /**
+     * 填写信息
+     */
+    async commitInfo() {
+      const { ctx, requestBody, loginPeopleId } = this.varBootstrap;
+      const peopleId = loginPeopleId;
+
+      const {
+        account_type,
+        op_type,
+        email,
+        opt_pan_card_number,
+        opt_name,
+        opt_phone,
+        opt_occupation,
+        name,
+        field,
+        second_category,
+        opt_registration_number,
+        opt_show_reference_url,
+        identity_type,
+        degree_of_completion,
+        media_address,
+        blog_address,
+        publish_article_address,
+        // 增加force,用于强制提交
+        force,
+        opt_pan_card_photo,
+        // 20180503 新增社媒字段 dser
+        linkedin_address,
+        facebook_address,
+        youtube_address,
+        instagram_address,
+        twitter_address,
+      } = requestBody;
+
+      /*
+       * 如果有 name 用户名敏感词检验 add by @欧阳炳成
+       * 敏感词检测分为大V敏感词以及敏感五害词，两者都要在注册账号的时候检测
+       */
+      if (name) {
+        const sensitiveRet = await this.sensitiveWordService
+          .checkSensitiveWord(name, 'name', account_type);
+        if (sensitiveRet.sensitive) {
+          this.statService.sendStat('node', {
+            tag: 'mobile_register_process_sensitive',
+            type: sensitiveRet.message,
+            sensitiveType: sensitiveRet.sensitiveType,
+            peopleId,
+            email,
+            name,
+          });
+          return ctx.errorRes(sensitiveRet.message, 20000);
+        }
+      }
+    }
+  };
+};
